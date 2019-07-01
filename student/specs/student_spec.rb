216abfea0692315217247f4799c114e56fd0805c
lawrie-sm <alertrelic@gmail.com>
2019-07-01 13:45:51 +0100

    Student class

--- student/specs/student_spec.rb
+++ student/specs/student_spec.rb
@@ -3,5 +3,8 @@ require("minitest/rg")
 require_relative("../student.rb")
 
 class TestStudent < MiniTest::Test
-
+  def test_student_name
+    student = Student.new("Ben", "E32")
+    assert_equal("Ben", student.name())
+  end
 end
